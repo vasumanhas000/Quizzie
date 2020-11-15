ddc30e8bf96f39341edf06f6fb28dd49cd5a9495
vasumanhas000 <vasumanhas000@gmail.com>
2020-11-15 22:21:04 +0530

    Added Quiz Screen

--- app/src/main/java/com/example/quizapp/HomePage.java
+++ app/src/main/java/com/example/quizapp/HomePage.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomePage extends AppCompatActivity {
+
+    public void startQuiz(View view){
+        Intent intent = new Intent(getApplicationContext(),QuizActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
